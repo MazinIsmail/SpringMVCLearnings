70b290053da28494c4d2dcefa070c64c3f6bc875
muthukumar s <muthukumar s@MLHKWSD03.fp.manulife.hk>
2017-07-05 16:55:05 +0800

    Controller Mappings

---
+++ src/main/java/org/springframework/samples/mvc/mapping/MappingController.java
@@ -0,0 +1,66 @@
+package org.springframework.samples.mvc.mapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/mapping/*")
+public class MappingController {
+
+	@RequestMapping("/path")
+	public @ResponseBody String byPath() {
+		return "Mapped by path!";
+	}
+
+	@RequestMapping(value="/path/*", method=RequestMethod.GET)
+	public @ResponseBody String byPathPattern(HttpServletRequest request) {
+		return "Mapped by path pattern ('" + request.getRequestURI() + "')";
+	}
+
+	@RequestMapping(value="/method", method=RequestMethod.GET)
+	public @ResponseBody String byMethod() {
+		return "Mapped by path + method";
+	}
+
+	@RequestMapping(value="/parameter", method=RequestMethod.GET, params="foo")
+	public @ResponseBody String byParameter() {
+		return "Mapped by path + method + presence of query parameter!";
+	}
+
+	@RequestMapping(value="/parameter", method=RequestMethod.GET, params="!foo")
+	public @ResponseBody String byParameterNegation() {
+		return "Mapped by path + method + not presence of query parameter!";
+	}
+
+	@RequestMapping(value="/header", method=RequestMethod.GET, headers="FooHeader=foo")
+	public @ResponseBody String byHeader() {
+		return "Mapped by path + method + presence of header!";
+	}
+
+	@RequestMapping(value="/header", method=RequestMethod.GET, headers="!FooHeader")
+	public @ResponseBody String byHeaderNegation() {
+		return "Mapped by path + method + absence of header!";
+	}
+
+	/*@RequestMapping(value="/consumes", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String byConsumes(@RequestBody JavaBean javaBean) {
+		return "Mapped by path + method + consumable media type (javaBean '" + javaBean + "')";
+	}*/
+
+	@RequestMapping(value="/produces", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody JavaBean byProducesJson() {
+		return new JavaBean();
+	}
+
+	@RequestMapping(value="/produces", method=RequestMethod.GET, produces=MediaType.APPLICATION_XML_VALUE)
+	public @ResponseBody JavaBean byProducesXml() {
+		return new JavaBean();
+	}
+
+}
